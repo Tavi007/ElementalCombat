d26a8ad686e075f59e0759919418001528e32bc5
Tavi007 <Tavi007@users.noreply.github.com>
2020-07-02 15:28:43 +0200

    Update ElementifyProjectileSpawnEvent

--- src/main/java/Tavi007/ElementalCombat/events/ElementifyProjectileSpawnEvent.java
+++ src/main/java/Tavi007/ElementalCombat/events/ElementifyProjectileSpawnEvent.java
@@ -8,6 +8,9 @@ import Tavi007.ElementalCombat.capabilities.IElementalAttackData;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,27 +23,43 @@ public class ElementifyProjectileSpawnEvent {
 	@SubscribeEvent
 	public static void elementifyProjectileSpawnEvent(EntityJoinWorldEvent event)
 	{
-		Entity entity = event.getEntity();
-		//check if entity is a new spawning projectile (only server side)
-		if(entity instanceof ProjectileEntity)
-		{
-			if(entity.ticksExisted == 0) //need checking
-			{
-				System.out.println("New Projectile");
-				/*
-				ProjectileEntity projectile = (ProjectileEntity) entity;
-				
-				//find source. (aka the held item by the nearest entity, that isn't the spawning entity?)
-				Vector3d pos = projectile.getPositionVec();
-				//List<Entity> entities = event.getWorld().getClosestEntity(e, predicate, null, pos.x, pos.y, pos.z);
-				//List<Entity> entities = event.getWorld().getEntitiesInAABBexcluding
-				ItemStack source = new ItemStack();
-				
-				//copy elemental attack capability
-				IElementalAttackData sourceData = ElementalCombatAPI.getElementalAttackData(source);
-				IElementalAttackData projectileData = ElementalCombatAPI.getElementalAttackData(projectile);
-				projectileData.setAttackMap(sourceData.getAttackMap());
-				*/
+		if(!event.getWorld().isRemote()){ // only server side should check
+			Entity entity = event.getEntity();
+			//check if entity is a new spawning projectile.
+			if(entity instanceof ProjectileEntity){
+				if(entity.ticksExisted == 0){
+					System.out.println("Newly spawned projectile found");
+					
+					ProjectileEntity projectile = (ProjectileEntity) entity;
+					
+					//find livingEntity source. (aka the nearest LivingEntity, that isn't the spawning projectileEntity? What about dispensers?)
+					Vector3d pos = projectile.getPositionVec();
+					AxisAlignedBB boundingBox = new AxisAlignedBB(pos.x, pos.y, pos.z, pos.x, pos.y, pos.z); 
+					List<Entity> nearbyEntities = event.getWorld().getEntitiesWithinAABBExcludingEntity(projectile, boundingBox);
+					System.out.println("Entity in List: "+ nearbyEntities);
+					
+					//check if only one entity was found. This should be the sourceEntity
+					//if List is empty, then source is a TileEntity (i.e Dispenser). 
+					if(nearbyEntities.size() == 1)
+					{
+						LivingEntity sourceEntity = (LivingEntity) nearbyEntities.get(0);
+						
+						// If sourceEntity holds an item, use item data.
+						// If not, use data from sourceEntity as default.
+						IElementalAttackData sourceData;
+						if(sourceEntity.hasItemInSlot(EquipmentSlotType.MAINHAND)){
+							ItemStack item = sourceEntity.getActiveItemStack();
+							sourceData = ElementalCombatAPI.getElementalAttackData(item);
+						}
+						else{
+							sourceData = ElementalCombatAPI.getElementalAttackData(sourceEntity);
+						}
+						
+						//copy elemental attack capability
+						IElementalAttackData projectileData = ElementalCombatAPI.getElementalAttackData(projectile);
+						projectileData.setAttackMap(sourceData.getAttackMap());
+					}
+				}
 			}
 		}
 	}
