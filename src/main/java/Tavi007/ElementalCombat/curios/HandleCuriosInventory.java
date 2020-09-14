442bcb0f1ca399a1151679acbca3a870adb2e9d8
Tavi007 <Tavi007@users.noreply.github.com>
2020-09-14 17:00:03 +0200

    some more clean up

--- src/main/java/Tavi007/ElementalCombat/curios/HandleCuriosInventory.java
+++ src/main/java/Tavi007/ElementalCombat/curios/HandleCuriosInventory.java
@@ -1,10 +1,7 @@
 package Tavi007.ElementalCombat.curios;
 
-import java.util.HashMap;
-
 import Tavi007.ElementalCombat.ElementalCombatAPI;
 import Tavi007.ElementalCombat.capabilities.defense.DefenseData;
-import Tavi007.ElementalCombat.util.DefenseDataHelper;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import top.theillusivec4.curios.api.CuriosApi;
 import net.minecraft.entity.LivingEntity;
@@ -13,8 +10,6 @@ import net.minecraft.item.ItemStack;
 public class HandleCuriosInventory {
 	public static DefenseData getDefenseData(LivingEntity entity) {
 		DefenseData defData = new DefenseData();
-		HashMap<String, Integer> styleFactor = defData.getStyleFactor();
-		HashMap<String, Integer> elementFactor = defData.getElementFactor();
 
 		IItemHandlerModifiable itemHandler = CuriosApi.getCuriosHelper().getEquippedCurios(entity).orElse(null);
 		if (itemHandler != null) {
@@ -26,8 +21,7 @@ public class HandleCuriosInventory {
 				{
 					// get defense data and add them up
 					DefenseData defDataItem = ElementalCombatAPI.getDefenseDataWithEnchantment(item);
-					DefenseDataHelper.sumMaps(styleFactor, defDataItem.getStyleFactor());
-					DefenseDataHelper.sumMaps(elementFactor, defDataItem.getElementFactor());
+					defData.sum(defDataItem);
 				}
 			}
 		}
--- src/main/java/Tavi007/ElementalCombat/events/ElementifyLivingEquipmentChange.java
+++ src/main/java/Tavi007/ElementalCombat/events/ElementifyLivingEquipmentChange.java
@@ -1,13 +1,10 @@
 package Tavi007.ElementalCombat.events;
 
-import java.util.HashMap;
-
 import Tavi007.ElementalCombat.ElementalCombat;
 import Tavi007.ElementalCombat.ElementalCombatAPI;
 import Tavi007.ElementalCombat.capabilities.defense.DefenseData;
 import Tavi007.ElementalCombat.curios.HandleCuriosInventory;
 import Tavi007.ElementalCombat.loading.EntityCombatProperties;
-import Tavi007.ElementalCombat.util.DefenseDataHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.util.ResourceLocation;
@@ -32,34 +29,25 @@ public class ElementifyLivingEquipmentChange
 			ResourceLocation rlEntity = entity.getType().getRegistryName();
 			ResourceLocation rlData = new ResourceLocation(ElementalCombat.MOD_ID, "entities/" + rlEntity.getNamespace() + "/" + rlEntity.getPath());
 			EntityCombatProperties entityData = ElementalCombat.COMBAT_PROPERTIES_MANGER.getEntityDataFromLocation(rlData);
-			HashMap<String, Integer> defenseStyle = new HashMap<String, Integer>(entityData.getDefenseStyle());
-			HashMap<String, Integer> defenseElement = new HashMap<String, Integer>(entityData.getDefenseElement());
-
+			
+			DefenseData defData = new DefenseData(entityData.getDefenseStyle(), entityData.getDefenseElement());
 			// get values from armor
 			entity.getArmorInventoryList().forEach(item -> {
 				if (!item.isEmpty()) {
 					DefenseData defCapItem = ElementalCombatAPI.getDefenseDataWithEnchantment(item);
-					HashMap<String, Integer> defenseStyleItem = defCapItem.getStyleFactor();
-					HashMap<String, Integer> defenseElementItem = defCapItem.getElementFactor();
-
-					DefenseDataHelper.sumMaps(defenseStyle, defenseStyleItem);
-					DefenseDataHelper.sumMaps(defenseElement, defenseElementItem);
+					defData.sum(defCapItem);
 				}
 			});
 			// cross-mod interaction with curios
 			if (ModList.get().isLoaded("curios")) {
 				DefenseData defCapItem = HandleCuriosInventory.getDefenseData(entity);
-				HashMap<String, Integer> defenseStyleItem = defCapItem.getStyleFactor();
-				HashMap<String, Integer> defenseElementItem = defCapItem.getElementFactor();
-
-				DefenseDataHelper.sumMaps(defenseStyle, defenseStyleItem);
-				DefenseDataHelper.sumMaps(defenseElement, defenseElementItem);
+				defData.sum(defCapItem);
 			}
 
 			// set values
 			DefenseData defCapEntity = ElementalCombatAPI.getDefenseData(entity);
-			defCapEntity.setStyleFactor(defenseStyle);
-			defCapEntity.setElementFactor(defenseElement);
+			defCapEntity.setStyleFactor(defData.getStyleFactor());
+			defCapEntity.setElementFactor(defData.getElementFactor());
 		}
 	}
 }
