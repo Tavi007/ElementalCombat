51156bc4ee1a03dd7ae58326bf4af3606f5427b0
Tavi007 <Tavi007@users.noreply.github.com>
2020-10-29 18:34:14 +0100

    fixed curios event not firing

--- src/main/java/Tavi007/ElementalCombat/curios/HandleCuriosInventory.java
+++ src/main/java/Tavi007/ElementalCombat/curios/HandleCuriosInventory.java
@@ -1,14 +1,17 @@
 package Tavi007.ElementalCombat.curios;
 
+import Tavi007.ElementalCombat.ElementalCombat;
 import Tavi007.ElementalCombat.ElementalCombatAPI;
 import Tavi007.ElementalCombat.capabilities.defense.DefenseData;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import top.theillusivec4.curios.api.event.CurioChangeEvent;
 
+@Mod.EventBusSubscriber(modid = ElementalCombat.MOD_ID, bus = Bus.FORGE)
 public class HandleCuriosInventory {
 	@SubscribeEvent
 	public static void onCurioChange(CurioChangeEvent event) {
-
 		// get Data
 		DefenseData defDataItemFrom = ElementalCombatAPI.getDefenseData(event.getFrom());
 		DefenseData defDataItemTo = ElementalCombatAPI.getDefenseData(event.getTo());
