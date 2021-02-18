ef09af8cea1167061081d88705b9c5507794cc96
Tavi007 <Tavi007@users.noreply.github.com>
2021-02-18 11:31:32 +0100

    added item properties for light bow animation
    more to come for other items

--- src/main/java/Tavi007/ElementalCombat/init/StartupClientOnly.java
+++ src/main/java/Tavi007/ElementalCombat/init/StartupClientOnly.java
@@ -7,6 +7,8 @@ import Tavi007.ElementalCombat.particle.CombatParticle;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.util.InputMappings;
+import net.minecraft.item.ItemModelsProperties;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,18 +16,31 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class StartupClientOnly {
-	
+
 	public static final KeyBinding TOGGLE_HUD = new KeyBinding("Toggle HUD", KeyConflictContext.UNIVERSAL, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_J, "Elemental Combat");
-	
+
 	@SubscribeEvent
 	public static void onClientSetupEvent(final FMLClientSetupEvent event)
 	{
 		//key bindings
 		ClientRegistry.registerKeyBinding(TOGGLE_HUD);
-		
+
+		//ItemProperties
+		ItemModelsProperties.registerProperty(ItemList.LIGHT_BOW.get(), new ResourceLocation("pull"), (stack, world, entity) -> {
+			if (entity == null) {
+				return 0.0F;
+			} else {
+				return entity.getActiveItemStack() != stack ? 0.0F : (float)(stack.getUseDuration() - entity.getItemInUseCount()) / 20.0F;
+			}
+		});
+		ItemModelsProperties.registerProperty(ItemList.LIGHT_BOW.get(), new ResourceLocation("pulling"), (stack, world, entity) -> {
+			return entity != null && entity.isHandActive() && entity.getActiveItemStack() == stack ? 1.0F : 0.0F;
+		});
+
+
 		ElementalCombat.LOGGER.info("ElementalCombat clientRegistries method registered.");	
 	}
-	
+
 	@SuppressWarnings("resource")
 	@SubscribeEvent
 	public static void onParticleFactoryRegistration(ParticleFactoryRegisterEvent event) {
@@ -33,11 +48,11 @@ public class StartupClientOnly {
 		Minecraft.getInstance().particles.registerFactory(ParticleList.ELEMENT_RESISTANCE.get(), CombatParticle.Factory::new);
 		Minecraft.getInstance().particles.registerFactory(ParticleList.ELEMENT_IMMUNITY.get(), CombatParticle.Factory::new);
 		Minecraft.getInstance().particles.registerFactory(ParticleList.ELEMENT_ABSORPTION.get(), CombatParticle.Factory::new);
-		
+
 		Minecraft.getInstance().particles.registerFactory(ParticleList.STYLE_WEAKNESS.get(), CombatParticle.Factory::new);
 		Minecraft.getInstance().particles.registerFactory(ParticleList.STYLE_RESISTANCE.get(), CombatParticle.Factory::new);
 		Minecraft.getInstance().particles.registerFactory(ParticleList.STYLE_IMMUNITY.get(), CombatParticle.Factory::new);
-		
-	    ElementalCombat.LOGGER.info("ElementalCombat particles factory registered.");
+
+		ElementalCombat.LOGGER.info("ElementalCombat particles factory registered.");
 	}
 }
