436374d9ec1fe4538ee4059c9b0fcfb4b96b0344
t.helmich <t.helmich@laemmerzahl.de>
2021-03-12 14:54:16 +0100

    fixed hud render
    and working on fixing ItemStack Tooltip

--- src/main/java/Tavi007/ElementalCombat/util/RenderHelper.java
+++ src/main/java/Tavi007/ElementalCombat/util/RenderHelper.java
@@ -1,9 +1,7 @@
 package Tavi007.ElementalCombat.util;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 
@@ -11,35 +9,34 @@ import Tavi007.ElementalCombat.api.attack.AttackData;
 import Tavi007.ElementalCombat.api.defense.DefenseData;
 import Tavi007.ElementalCombat.config.ClientConfig;
 import Tavi007.ElementalCombat.ElementalCombat;
-import Tavi007.ElementalCombat.api.DefaultPropertiesAPI;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 
 public class RenderHelper {
-	
+
 	public static final int iconSize = 8;
-	
+	public static final int maxLineHeight = Math.max(Minecraft.getInstance().fontRenderer.FONT_HEIGHT, iconSize);
+	public static final int maxLineWidth = Minecraft.getInstance().fontRenderer.getStringWidth("Defense: -999%") + iconSize + 2;
+
 	private static int iteratorCounter=0;
-	
+
 	public static void tickIteratorCounter() {
 		iteratorCounter++;
-		
+
 		if(iteratorCounter == Integer.MAX_VALUE || iteratorCounter < 0) {
 			iteratorCounter = 0;
 		}
 	}
-	
+
 	public static int getInteratorCounter() {
 		return iteratorCounter;
 	}
-	
+
 	public static void render(AttackData attackData, MatrixStack matrixStack, float posX, float posY) {
 		Minecraft mc = Minecraft.getInstance();
-		
+
 		// caption
 		if(ClientConfig.textShadow()) {
 			mc.fontRenderer.drawStringWithShadow(matrixStack, "Attack:", posX, posY, TextFormatting.GRAY.getColor());
@@ -47,13 +44,12 @@ public class RenderHelper {
 		else {
 			mc.fontRenderer.drawStringWithShadow(matrixStack, "Attack:", posX, posY, TextFormatting.GRAY.getColor());
 		}
-		posY += mc.fontRenderer.FONT_HEIGHT;
-		posX += 2;
+		posX += mc.fontRenderer.getStringWidth("Attack: ");
 
 		// icon element
 		mc.getTextureManager().bindTexture(new ResourceLocation(ElementalCombat.MOD_ID, "textures/icons/" + attackData.getElement() + ".png"));
 		AbstractGui.blit(matrixStack, (int) posX, (int) posY, 0, 0, iconSize, iconSize, iconSize, iconSize);
-		posX += iconSize;
+		posX += iconSize + 1;
 
 		// icon style
 		mc.getTextureManager().bindTexture(new ResourceLocation(ElementalCombat.MOD_ID, "textures/icons/" + attackData.getStyle() + ".png"));
@@ -65,7 +61,7 @@ public class RenderHelper {
 
 	public static void render(DefenseData defData, MatrixStack matrixStack, float posX, float posY) {
 		Minecraft mc = Minecraft.getInstance();
-		
+
 		// caption
 		if(ClientConfig.textShadow()) {
 			mc.fontRenderer.drawStringWithShadow(matrixStack, "Defense:", posX, posY, TextFormatting.GRAY.getColor());
@@ -73,9 +69,8 @@ public class RenderHelper {
 		else {
 			mc.fontRenderer.drawStringWithShadow(matrixStack, "Defense:", posX, posY, TextFormatting.GRAY.getColor());
 		}
-		posY += mc.fontRenderer.FONT_HEIGHT;
-		posX += 2;
-		
+		posX += mc.fontRenderer.getStringWidth("Defense: ");
+
 		// icon
 		LinkedHashMap<String, Integer> map = new LinkedHashMap<String, Integer>();
 		map.putAll(defData.getElementFactor());
@@ -83,15 +78,15 @@ public class RenderHelper {
 		String key = (new ArrayList<String>(map.keySet())).get(iteratorCounter % map.size());
 		mc.getTextureManager().bindTexture(new ResourceLocation(ElementalCombat.MOD_ID, "textures/icons/" + key + ".png"));
 		AbstractGui.blit(matrixStack, (int) posX, (int) posY, 0, 0, iconSize, iconSize, iconSize, iconSize);
-		
+
 		// value
-		posX += iconSize + 2;
+		posX -= mc.fontRenderer.getStringWidth("Defense: ");
 		renderPercentage(key, map.get(key), matrixStack, posX, posY);
 
 		// reset to default texture
 		mc.getTextureManager().bindTexture(AbstractGui.GUI_ICONS_LOCATION);
 	}
-	
+
 	/**
 	 * A Helper-function for constructing a formatted string. Used for tooltips and hud. (See {@link Tavi007.ElementalCombat.events.RenderEvents})
 	 * @param key The Name of the value. The key can be from the element or style defense mapping.
@@ -107,13 +102,17 @@ public class RenderHelper {
 		if (percentage == 100) {textFormatting = TextFormatting.YELLOW;}
 		if (percentage > 100) {textFormatting = TextFormatting.GREEN;}
 
-		// write text
+		// bound to the right side
 		Minecraft mc = Minecraft.getInstance();
+		String percentageString = String.valueOf(percentage) + "%";
+		posX += maxLineWidth - mc.fontRenderer.getStringWidth(percentageString);
+		
+		// write text
 		if(ClientConfig.textShadow()) {
-			mc.fontRenderer.drawStringWithShadow(matrixStack, String.valueOf(percentage) + "%" , posX, posY, textFormatting.getColor());
+			mc.fontRenderer.drawStringWithShadow(matrixStack,  percentageString, posX, posY, textFormatting.getColor());
 		}
 		else {
-			mc.fontRenderer.drawStringWithShadow(matrixStack, String.valueOf(percentage) + "%" , posX, posY, textFormatting.getColor());
+			mc.fontRenderer.drawStringWithShadow(matrixStack, percentageString, posX, posY, textFormatting.getColor());
 		}
 	}
 }
