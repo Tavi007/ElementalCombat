64d9f98795f35570ca3a89b9b6131d7e1af6ceb3
Tavi007 <Tavi007@users.noreply.github.com>
2021-08-01 17:28:04 +0200

    Fixed error with curios

--- src/main/java/Tavi007/ElementalCombat/interaction/HandleCuriosInventory.java
+++ src/main/java/Tavi007/ElementalCombat/interaction/HandleCuriosInventory.java
@@ -3,6 +3,7 @@ package Tavi007.ElementalCombat.interaction;
 import Tavi007.ElementalCombat.api.DefenseDataAPI;
 import Tavi007.ElementalCombat.api.defense.DefenseData;
 import Tavi007.ElementalCombat.api.defense.DefenseLayer;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import top.theillusivec4.curios.api.event.CurioChangeEvent;
@@ -10,18 +11,23 @@ import top.theillusivec4.curios.api.event.CurioChangeEvent;
 public class HandleCuriosInventory {
 	@SubscribeEvent
 	public static void onCurioChange(CurioChangeEvent event) {
+		LivingEntity entity = event.getEntityLiving();
+		DefenseData data = DefenseDataAPI.get(entity);
+		ResourceLocation rl = new ResourceLocation("curios", "armor");
+		DefenseLayer layer = data.getLayer(rl);
+		if(layer == null) {
+			layer = new DefenseLayer();
+		}
+		
 		// get data
 		DefenseData defDataItemFrom = DefenseDataAPI.get(event.getFrom());
 		DefenseData defDataItemTo = DefenseDataAPI.get(event.getTo());
 
 		// compute change
-		DefenseLayer layer = new DefenseLayer();
 		layer.subtractLayer(defDataItemFrom.toLayer());
 		layer.addLayer(defDataItemTo.toLayer());
 
 		// apply change
-		DefenseData data = DefenseDataAPI.get(event.getEntityLiving());
-		ResourceLocation rl = new ResourceLocation("curios", "armor");
-		data.getLayer(rl).addLayer(layer);
+		DefenseDataAPI.putLayer(entity, layer, rl);
 	}
 }
