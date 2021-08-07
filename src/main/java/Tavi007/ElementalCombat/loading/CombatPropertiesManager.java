6e7d0a3d7ca17b89ca0d6a3aa90ba68342329f22
Tavi007 <Tavi007@users.noreply.github.com>
2021-08-07 19:13:58 +0200

    some proper logging

--- src/main/java/Tavi007/ElementalCombat/loading/CombatPropertiesManager.java
+++ src/main/java/Tavi007/ElementalCombat/loading/CombatPropertiesManager.java
@@ -1,6 +1,7 @@
 package Tavi007.ElementalCombat.loading;
 
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.Nullable;
@@ -45,6 +46,16 @@ public class CombatPropertiesManager extends JsonReloadListener
 		this.registeredBiomeData = message.getBiomeData();
 		this.registeredProjectileData = message.getProjectileData();
 		this.registeredDamageSourceData = message.getDamageSourceData();
+		
+		logLoading("client", registeredMobData.size(), "mobs");
+		logLoading("client", registeredItemData.size(), "items");
+		logLoading("client", registeredBiomeData.size(), "biomes");
+		logLoading("client", registeredProjectileData.size(), "projectiles");
+		logLoading("client", registeredDamageSourceData.size(), "damage sources");
+	}
+	
+	private void logLoading(String side, int size, String type) {
+		ElementalCombat.LOGGER.info(side + " loaded " + size + " combat properties for " + type);
 	}
 
 	public BasePropertiesMessage createSyncMessage() {
@@ -63,30 +74,47 @@ public class CombatPropertiesManager extends JsonReloadListener
 			ElementalCombat.LOGGER.warn("Datapack tried to redefine {} elemental entity data, ignoring", (Object)EMPTY_RESOURCELOCATION);
 		}
 
+		Map<String, Map<String, Integer>> counter = new HashMap<String, Map<String, Integer>>();
 		objectIn.forEach((rl, json) -> 
 		{
 			try (net.minecraft.resources.IResource res = resourceManagerIn.getResource(getPreparedPath(rl));)
 			{
+				String modid = rl.getNamespace();
+				String type = "incorrect entries (check path!)";
 				//check if entity/item/biome/damageSource gets loaded
 				if(rl.getPath().contains("mobs/")){
 					MobCombatProperties combatProperties = loadData(GSON, rl, json, res == null || !res.getPackName().equals("main"), MobCombatProperties.class);
 					builderMob.put(rl, combatProperties);
+					type = "mobs";
 				}
 				else if(rl.getPath().contains("items/")){
 					ItemCombatProperties combatProperties = loadData(GSON, rl, json, res == null || !res.getPackName().equals("main"), ItemCombatProperties.class);
 					builderItem.put(rl, combatProperties);
+					type = "items";
 				}
 				else if(rl.getPath().contains("biomes/")){
 					BiomeCombatProperties combatProperties = loadData(GSON, rl, json, res == null || !res.getPackName().equals("main"), BiomeCombatProperties.class);
 					builderBiome.put(rl, combatProperties);
+					type = "biomes";
 				}
 				else if(rl.getPath().contains("damage_sources/")){
 					AttackOnlyCombatProperties combatProperties = loadData(GSON, rl, json, res == null || !res.getPackName().equals("main"), AttackOnlyCombatProperties.class);
 					builderDamageSource.put(rl, combatProperties);
+					type = "damage_sources";
 				}
 				else if(rl.getPath().contains("projectiles/")){
 					AttackOnlyCombatProperties combatProperties = loadData(GSON, rl, json, res == null || !res.getPackName().equals("main"), AttackOnlyCombatProperties.class);
 					builderProjectile.put(rl, combatProperties);
+					type = "projectiles";
+				}
+				
+				Map<String, Integer> propertyCounter = counter.get(modid);
+				if(propertyCounter == null) {
+					propertyCounter = new HashMap<String, Integer>();
+					propertyCounter.put(type, 1);
+					counter.put(modid, propertyCounter);
+				} else {
+					propertyCounter.put(type, propertyCounter.get(type) + 1);
 				}
 			}
 			catch (Exception exception)
@@ -110,6 +138,20 @@ public class CombatPropertiesManager extends JsonReloadListener
 
 		builderProjectile.put(EMPTY_RESOURCELOCATION, new AttackOnlyCombatProperties());
 		this.registeredProjectileData = builderProjectile.build();
+		
+		
+		counter.forEach( (modid, propertyCounter) -> {
+			ElementalCombat.LOGGER.info("The mod " + modid + " loaded: ");
+			propertyCounter.forEach( (type,amount) -> {
+				ElementalCombat.LOGGER.info("" + amount + " " + type);
+			});
+		});
+
+		logLoading("server", registeredMobData.size(), "mobs");
+		logLoading("server", registeredItemData.size(), "items");
+		logLoading("server", registeredBiomeData.size(), "biomes");
+		logLoading("server", registeredProjectileData.size(), "projectiles");
+		logLoading("server", registeredDamageSourceData.size(), "damage sources");
 	}
 
 	@Nullable
