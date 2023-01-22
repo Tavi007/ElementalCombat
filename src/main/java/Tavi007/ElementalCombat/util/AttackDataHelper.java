f59479c475dc1db2421132e418edc917ce98cd47
Tavi007 <Tavi007@users.noreply.github.com>
2023-01-22 16:00:50 +0100

    fixed imports

--- src/main/java/Tavi007/ElementalCombat/util/AttackDataHelper.java
+++ src/main/java/Tavi007/ElementalCombat/util/AttackDataHelper.java
@@ -4,11 +4,13 @@ import Tavi007.ElementalCombat.api.AttackDataAPI;
 import Tavi007.ElementalCombat.api.BasePropertiesAPI;
 import Tavi007.ElementalCombat.capabilities.attack.AttackData;
 import Tavi007.ElementalCombat.capabilities.attack.AttackDataCapability;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
 public class AttackDataHelper {
 
@@ -48,7 +50,7 @@ public class AttackDataHelper {
         return attackData;
     }
 
-    public static AttackData get(net.minecraft.util.DamageSource damageSource) {
+    public static AttackData get(DamageSource damageSource) {
         Entity immediateSource = damageSource.getDirectEntity();
 
         // Get combat data from source
