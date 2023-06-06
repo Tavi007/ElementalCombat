263ac5f2fd578b5b0bc285c9f728720e0d49f51f
Tavi007 <Tavi007@users.noreply.github.com>
2023-06-06 21:19:54 +0200

    death message hotfix of elemental harming damage source

--- src/main/java/Tavi007/ElementalCombat/potions/ElementalHarmingEffect.java
+++ src/main/java/Tavi007/ElementalCombat/potions/ElementalHarmingEffect.java
@@ -25,7 +25,7 @@ public class ElementalHarmingEffect extends MobEffect {
     @Override
     public void applyInstantenousEffect(@Nullable Entity potionEntity, @Nullable Entity potionEntityOwner, LivingEntity target, int level, double distance) {
         int damage = (int) (distance * (double) (6 << level) + 0.5D);
-        target.hurt(new HarmingEffectDamageSource("harming." + element), (float) damage);
+        target.hurt(new HarmingEffectDamageSource("magic." + element), (float) damage);
     }
 
     @Override
