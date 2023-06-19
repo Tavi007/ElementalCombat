fa3429e2ada9907ec684a7277b213dbb4099d1ad
Tavi007 <Tavi007@users.noreply.github.com>
2023-06-19 13:20:54 +0200

    forgot the import

--- src/main/java/Tavi007/ElementalCombat/potions/ElementalHarmingEffect.java
+++ src/main/java/Tavi007/ElementalCombat/potions/ElementalHarmingEffect.java
@@ -3,6 +3,7 @@ package Tavi007.ElementalCombat.potions;
 import javax.annotation.Nullable;
 
 import Tavi007.ElementalCombat.capabilities.attack.AttackLayer;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.Entity;
