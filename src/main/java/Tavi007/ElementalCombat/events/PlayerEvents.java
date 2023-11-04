c84bd1dd53de7b269c486d1b77b5a0ec37727272
Tavi007 <Tavi007@users.noreply.github.com>
2023-11-04 19:38:35 +0100

    An event decides if the defense values of an item are added to the entity

--- src/main/java/Tavi007/ElementalCombat/events/PlayerEvents.java
+++ src/main/java/Tavi007/ElementalCombat/events/PlayerEvents.java
@@ -2,17 +2,22 @@ package Tavi007.ElementalCombat.events;
 
 import Tavi007.ElementalCombat.ElementalCombat;
 import Tavi007.ElementalCombat.api.DefenseDataAPI;
-import Tavi007.ElementalCombat.capabilities.defense.DefenseData;
-import Tavi007.ElementalCombat.capabilities.defense.DefenseLayer;
+import Tavi007.ElementalCombat.api.GainDefenseFromEquipmentEvent;
 import Tavi007.ElementalCombat.config.ClientConfig;
 import Tavi007.ElementalCombat.init.StartupClientOnly;
 import Tavi007.ElementalCombat.util.AttackDataHelper;
 import Tavi007.ElementalCombat.util.DefenseDataHelper;
 import Tavi007.ElementalCombat.util.NetworkHelper;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ShieldItem;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.EnchantedBookItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PotionItem;
 import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,26 +33,31 @@ public class PlayerEvents {
         if (entity == null) {
             return;
         }
-        // change defense properties
-        DefenseLayer defenseLayer = new DefenseLayer();
-        switch (event.getSlot().getType()) {
-        case ARMOR:
-            entity.getArmorSlots().forEach(stack -> {
-                DefenseData data = DefenseDataHelper.get(stack);
-                defenseLayer.addLayer(data.toLayer());
-            });
-            DefenseDataAPI.putLayer(entity, defenseLayer, new ResourceLocation("armor"));
-        case HAND:
+
+        if (EquipmentSlot.MAINHAND.equals(event.getSlot())) {
             AttackDataHelper.updateItemLayer(entity);
-            if (entity.getOffhandItem().getItem() instanceof ShieldItem) {
-                defenseLayer.addLayer(DefenseDataHelper.get(entity.getOffhandItem()).toLayer());
-            }
-            if (entity.getMainHandItem().getItem() instanceof ShieldItem) {
-                defenseLayer.addLayer(DefenseDataHelper.get(entity.getOffhandItem()).toLayer());
-            }
-            if (!defenseLayer.isEmpty()) {
-                DefenseDataAPI.putLayer(entity, defenseLayer, new ResourceLocation("hands"));
-            }
+        }
+
+        GainDefenseFromEquipmentEvent gainDefenseEvent = new GainDefenseFromEquipmentEvent(
+            event.getTo(),
+            event.getSlot());
+        MinecraftForge.EVENT_BUS.post(gainDefenseEvent);
+        if (!gainDefenseEvent.isCanceled()) {
+            DefenseDataAPI.putLayer(
+                entity,
+                DefenseDataHelper.get(event.getTo()).toLayer(),
+                new ResourceLocation(event.getSlot().name().toLowerCase()));
+        }
+    }
+
+    @SubscribeEvent
+    public static void gainDefenseFromEquipmentEvent(GainDefenseFromEquipmentEvent event) {
+        if (EquipmentSlot.Type.HAND.equals(event.getEquipmentSlot().getType())) {
+            ItemStack stack = event.getItemStack();
+            Item item = stack.getItem();
+            event.setCanceled(item instanceof ArmorItem ||
+                item instanceof PotionItem ||
+                item instanceof EnchantedBookItem);
         }
     }
 
