485bdc2c972cbab548a73e3923f3c735bbf4fd2c
Tavi007 <Tavi007@users.noreply.github.com>
2023-11-04 20:11:52 +0100

    added an event that decides if the defense values of equipped item are added to the entity

--- src/main/java/Tavi007/ElementalCombat/compatibility/curios/HandleCuriosInventory.java
+++ src/main/java/Tavi007/ElementalCombat/compatibility/curios/HandleCuriosInventory.java
@@ -1,13 +1,16 @@
 package Tavi007.ElementalCombat.compatibility.curios;
 
 import Tavi007.ElementalCombat.api.DefenseDataAPI;
+import Tavi007.ElementalCombat.api.GainDefenseFromEquipmentEvent;
 import Tavi007.ElementalCombat.capabilities.defense.DefenseData;
 import Tavi007.ElementalCombat.capabilities.defense.DefenseLayer;
 import Tavi007.ElementalCombat.util.DefenseDataHelper;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import top.theillusivec4.curios.api.event.CurioChangeEvent;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 public class HandleCuriosInventory {
 
@@ -29,5 +32,12 @@ public class HandleCuriosInventory {
         layer.subtractLayer(defDataItemFrom.toLayer());
         layer.addLayer(defDataItemTo.toLayer());
         DefenseDataAPI.putLayer(entity, layer, rl);
+    }
+
+    @SubscribeEvent
+    public static void gainDefenseFromEquipmentEvent(GainDefenseFromEquipmentEvent event) {
+        if (EquipmentSlot.Type.HAND.equals(event.getEquipmentSlot().getType())) {
+            event.setCanceled(event.getItemStack().getItem() instanceof ICurioItem);
+        }
     }
 }
