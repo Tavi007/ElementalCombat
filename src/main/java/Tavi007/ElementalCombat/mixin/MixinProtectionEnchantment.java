fc67c7052e6cf3aa1ab46f9c3d33850ed132d590
Tavi007 <Tavi007@users.noreply.github.com>
2024-06-23 16:20:16 +0200

    use mixin instead of overrding enchantment

---
+++ src/main/java/Tavi007/ElementalCombat/mixin/MixinProtectionEnchantment.java
@@ -0,0 +1,31 @@
+package Tavi007.ElementalCombat.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.item.enchantment.ProtectionEnchantment;
+
+@Mixin(ProtectionEnchantment.class)
+public class MixinProtectionEnchantment {
+
+    @Inject(at = @At("HEAD"), method = "getDamageProtection(ILnet/minecraft/util/DamageSource;)I", cancellable = true)
+    private void getDamageProtection(int level, DamageSource damageSource, CallbackInfoReturnable<Integer> callback) {
+        if (damageSource.isBypassInvul()) {
+            callback.setReturnValue(0);
+        } else if (this.type == ProtectionEnchantment.Type.FIRE && damageSource.isFire()) {
+            callback.setReturnValue(0);
+        } else if (this.type == ProtectionEnchantment.Type.EXPLOSION && damageSource.isExplosion()) {
+            callback.setReturnValue(0);
+        } else if (this.type == ProtectionEnchantment.Type.PROJECTILE && damageSource.isProjectile()) {
+            callback.setReturnValue(0);
+        }
+    }
+
+    @Shadow
+    private ProtectionEnchantment.Type type;
+
+}
